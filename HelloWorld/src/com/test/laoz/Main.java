fef47058dc95f8a095f50759ca3ed84cc3394d68
zhaosongtao163 <zhaosongtao163@163.com>
2018-11-02 14:17:23 +0800

    add

---
+++ HelloWorld/src/com/test/laoz/Main.java
@@ -0,0 +1,21 @@
+package com.test.laoz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("2");
+		List<String> list = new ArrayList<>();
+		list.add("aaa");
+		list.add("aaa");
+		list.add("aaa");
+		list.add("aaa");
+		list.add("aaa");
+		System.out.println(list);
+	}
+
+}
